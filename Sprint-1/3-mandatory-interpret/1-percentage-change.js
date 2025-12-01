81571e499d611d36d8a44b6575968ad0f29787cc
Xinxi <xinxichen1022@gmail.com>
2025-12-01 17:20:26 +0100

    3-to-pounds done

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,15 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+    //There are 6 function calls in this file.
+    //Line 4: Number() and replaceAll()
+    //Line 5: Number() and replaceAll()
+    //Line 8: console.log() and ${}//
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
+    //The error is on Line 5, it lacks of ',' between "," ""//
 // c) Identify all the lines that are variable reassignment statements
-
+    //Line 4, Line 5//
 // d) Identify all the lines that are variable declarations
-
+    //Line 1, Line 2, Line 7, Line 8//
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+    //It converts the string to Number//
