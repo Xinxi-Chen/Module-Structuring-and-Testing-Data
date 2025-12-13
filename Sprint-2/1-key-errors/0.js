4ebe3541bd66942b4ecf697e75d87e0ee6b64e8c
Xinxi <xinxichen1022@gmail.com>
2025-12-13 18:30:08 +0100

    1-key-errors completed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 //  =============> write your prediction here
+There is a syntax error. Str is a parameter, then it has declared again as let str =.
+Identifier 'str' has already been declared.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
@@ -10,4 +12,9 @@ function capitalise(str) {
 }
 
 // =============> write your explanation here
+//
 // =============> write your new code here
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,22 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+The parameter 3 is an error.
+
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+function square(3)
 // =============> explain this error message here
-
+The parameter should 2 is a problem. It should be num, otherwise there is no declaration of num.
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+    return num * num;
+}
+console.log(square(3));
