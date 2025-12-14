2834f5d3119d4a6bcb6ded102b8e64e41ae39d85
Xinxi <xinxichen1022@gmail.com>
2025-12-14 09:27:50 +0100

    3-mandatory-implement completed

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -16,4 +16,7 @@
 
 function calculateBMI(weight, height) {
     // return the BMI of someone based off their weight and height
+    const bmi = weight / (height * height);
+    return bmi.toFixed(1);
 }
+console.log(calculateBMI(70, 1.73));
