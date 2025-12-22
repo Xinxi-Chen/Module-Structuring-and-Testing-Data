e7ff019f7b22530603b59eb33da2c94fea0dd9f8
Xinxi <xinxichen1022@gmail.com>
2025-12-22 12:04:40 +0100

    2-practice-tdd completed

--- Sprint-3/2-practice-tdd/repeat-str.js
+++ Sprint-3/2-practice-tdd/repeat-str.js
@@ -1,5 +1,13 @@
-function repeatStr() {
-  return "hellohellohello";
+function repeatStr(str, count) {
+  if (count < 0) {
+    throw new Error("Count must be a non-negative integer.");
+  }
+  if (count === 0) {
+    return "";
+  }
+  if (count === 1) {
+    return str;
+  }
+  return str.repeat(count);
 }
-
 module.exports = repeatStr;
--- Sprint-3/2-practice-tdd/repeat-str.test.js
+++ Sprint-3/2-practice-tdd/repeat-str.test.js
@@ -20,13 +20,28 @@ test("should repeat the string count times", () => {
 // Given a target string str and a count equal to 1,
 // When the repeatStr function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
-
+test("should return the original str without repetition", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeatStr function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
-
+test("should return an empty string", () => {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeatStr(str, count);
+  expect(repeatedStr).toEqual("");
+});
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeatStr function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+test("should an appropriate error message", () => {
+  const str = "hello";
+  const count = -5;
+  expect(() => repeatedStr(str, count)).toThrow(Error);
+});
